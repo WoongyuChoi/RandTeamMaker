f7353612e30fceac6c0644441f6cf276eeb25add
WoongyuChoi <schna@kakao.com>
2025-04-14 18:21:18 +0900

    feature : Add favicon.ico

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication
-from rand_team_maker.gui import RandTeamMakerApp
 
 # 현재 디렉터리를 Python 모듈 경로에 추가
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from rand_team_maker.gui import RandTeamMakerApp
+
 
 def main():
     app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("favicon.ico"))
+
     window = RandTeamMakerApp()
     window.show()
     sys.exit(app.exec_())
